7a745c8189643d2ae091098900b7f4e3e277392f
Stephen Smith <stesmith@zulily.com>
2019-11-25 17:27:55 -0800

    Initial Commit

---
+++ LayoutGuides/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutGuides
+//
+//  Created by Stephen Smith on 11/25/19.
+//  Copyright © 2019 Zulily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
