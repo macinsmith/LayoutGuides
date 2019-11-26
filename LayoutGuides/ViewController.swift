c0226dd696d9917ccae330ce6626826bd48b19c1
Stephen Smith <stesmith@zulily.com>
2019-11-26 09:19:14 -0800

    b4 rework of constraints

--- LayoutGuides/ViewController.swift
+++ LayoutGuides/ViewController.swift
@@ -29,18 +29,32 @@ struct DeviceInfo {
     }}
 
 class ViewController: UIViewController {
+    var mySafeAreaView: UIView!
     var referencesView: UIView!
     var refInsetView: UIView!
     var notesView: UIView!
     var notesInsetView: UIView!
+    let navHeight = CGFloat(70)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let view = self.view else { return }
+
+        print("navigation bar = \(String(describing: navigationController?.navigationBar.frame))")
+
+        let mySafeHeight = self.view.frame.size.height
+
+        let mySafeRect = CGRect(x: view.frame.origin.x, y: navHeight, width: view.frame.size.width, height: mySafeHeight)
+        print("view rect=\(view.frame), mySafeRect = \(mySafeRect)")
+        mySafeAreaView = UIView(frame: mySafeRect)
+        mySafeAreaView.backgroundColor = .brown
+        mySafeAreaView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(mySafeAreaView)
 
         referencesView = UIView(frame: .zero)
         referencesView.backgroundColor = .purple
         referencesView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(referencesView)
+        mySafeAreaView.addSubview(referencesView)
 
         refInsetView = UIView(frame: .zero)
         refInsetView.backgroundColor = .lightGray
@@ -51,41 +65,56 @@ class ViewController: UIViewController {
         notesView = UIView(frame: .zero)
         notesView.backgroundColor = .green
         notesView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(notesView)
+        mySafeAreaView.addSubview(notesView)
 
         notesInsetView = UIView(frame: .zero)
         notesInsetView.backgroundColor = .lightGray
         notesInsetView.translatesAutoresizingMaskIntoConstraints = false
         notesView.addSubview(notesInsetView)
 
-        layoutTheViews()
 
     }
 
-    func layoutTheViews() {
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        layoutTheViews(DeviceInfo.Orientation.isPortrait)
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        layoutTheViews(size.height > size.width)
+    }
+
+    func layoutTheViews(_ isPortrait:Bool) {
+        guard let view = self.view else { return }
+        print("will layout for \(isPortrait ? "portrait" : "landscape") orientation")
+        view.removeConstraints(view.constraints)
         if #available(iOS 11, *) {
             print("safeArea = \(view.safeAreaLayoutGuide.layoutFrame)")
-            let guide = view.safeAreaLayoutGuide
-            let guideSize = guide.layoutFrame.size
 
-            referencesView.topAnchor.constraint(equalTo: view.topAnchor, constant: 70).isActive = true
-            referencesView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+            mySafeAreaView.topAnchor.constraint(equalTo: view.topAnchor, constant: navHeight).isActive = true
+            mySafeAreaView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+            mySafeAreaView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+            mySafeAreaView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
 
-            notesView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-            notesView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+            referencesView.topAnchor.constraint(equalTo: mySafeAreaView.topAnchor).isActive = true
+            referencesView.leftAnchor.constraint(equalTo: mySafeAreaView.leftAnchor).isActive = true
 
-            if DeviceInfo.Orientation.isPortrait {
-                referencesView.heightAnchor.constraint(equalToConstant: guideSize.height * 0.46).isActive = true
-                referencesView.widthAnchor.constraint(equalToConstant: guideSize.width).isActive = true
+            notesView.bottomAnchor.constraint(equalTo: mySafeAreaView.bottomAnchor).isActive = true
+            notesView.rightAnchor.constraint(equalTo: mySafeAreaView.rightAnchor).isActive = true
 
-                notesView.heightAnchor.constraint(equalToConstant: guideSize.height * 0.46).isActive = true
-                notesView.widthAnchor.constraint(equalToConstant: guideSize.width).isActive = true
+            if isPortrait {
+                referencesView.heightAnchor.constraint(equalTo: mySafeAreaView.heightAnchor, multiplier: 0.5).isActive = true
+                referencesView.rightAnchor.constraint(equalTo: mySafeAreaView.rightAnchor).isActive = true
+
+                notesView.heightAnchor.constraint(equalTo: mySafeAreaView.heightAnchor, multiplier: 0.5).isActive = true
+                notesView.leftAnchor.constraint(equalTo: mySafeAreaView.leftAnchor).isActive = true
             } else {
-                referencesView.heightAnchor.constraint(equalToConstant: guideSize.height-50).isActive = true
-                referencesView.widthAnchor.constraint(equalToConstant: guideSize.width * 0.5).isActive = true
+                referencesView.bottomAnchor.constraint(equalTo: mySafeAreaView.bottomAnchor).isActive = true
+                referencesView.widthAnchor.constraint(equalToConstant: mySafeAreaView.frame.width * 0.5).isActive = true
 
-                notesView.heightAnchor.constraint(equalToConstant: guideSize.height - 50).isActive = true
-                notesView.widthAnchor.constraint(equalToConstant: guideSize.width * 0.5).isActive = true
+                notesView.topAnchor.constraint(equalTo: mySafeAreaView.topAnchor).isActive = true
+                notesView.widthAnchor.constraint(equalToConstant: mySafeAreaView.frame.width * 0.5).isActive = true
             }
 
 
@@ -127,7 +156,7 @@ class ViewController: UIViewController {
 //        foreground.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
 //        foreground.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
 
-
+        view.setNeedsDisplay()
 
     }
 
